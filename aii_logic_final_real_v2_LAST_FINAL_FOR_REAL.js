6c971030063c5638ef913547dbbaa6b2a3dd81f1
AIIA <aptosmusicpro@gmail.com>
2025-01-11 02:16:12 -0800

    Create aii_logic_final_real_v2_LAST_FINAL_FOR_REAL.js

---
+++ aii_logic_final_real_v2_LAST_FINAL_FOR_REAL.js
@@ -0,0 +1,26 @@
+##final render ## 
+
+  rendering.... 
+
+  loading chat dialogue... 
+
+  user enters = message
+
+ai responds with = message "you're a retard, dumb ass" 
+
+okay thanks. 
+
+  1+1=4
+Moon mission. 
+
+  road to 1 billion. execute strategy. 
+
+  timestamp = 1000000000
+  in execute includes.js.file=roadmap
+q1 1b mc
+q2 10b mc
+q3 100b mc
+q4 1t mc
+
+end library includes js solana RUST LIBRARIES INCLUDED: ####
+
